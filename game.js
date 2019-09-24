e134b1a86a873c7b86ca0e005ea528935fd80e86
Harrison Dailey <hfdail79>
2019-09-24 09:51:36 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,24 +1,29 @@
-var bread = 0;
-var pigeons = 0;
-var power = 1;
-var price = 10
+var breadtrash = 1;
+var pigeons = 1;
+
 
 var title = new Title('Bird Up'); // Create a title element
-var feed = new Button('Tame Pigeon', btn2Press);   // Create a button element
+var feed = new Button('Feed Pigeon', btnPress);   // Create a button element
 
-var sfb = new Button('Scavenge for bread', btnPress);
-var bc = new Text('bread');
+var sfb = new Text('Trash bread: '+ breadtrash );
+
+
+var row1 = new Section('row'); 
+var column1 = new Section('col');
+var column2 = new Section('col');
+column1.add(sfb);
+column2.add(feed);
+
+row1.add(sfb);
+row1.add(feed);
+
+
 
 function btnPress() {
-  bread = bread + power
+  if(breadtrash > 0){
+    pigeons = pigeons + 1
+    breadtrash = breadtrash - 1
+  }
   
 }
 
-
-function btn2Press(){
-  if(bread >= price){
-  
-  bread = (bread - price)
-  pigeons = (pigeons + pigeons / 5) 
-}
-}
